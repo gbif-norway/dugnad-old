2e02a82f1037da3ef785af5eb32454fdcc6f363c
bie <bie@blekksprut.net>
2016-08-15 15:12:12 +0900

    ugh

---
+++ dugnad.py
@@ -0,0 +1,136 @@
+#!/usr/bin/python
+# coding: utf-8
+
+# todo:
+# - frie termer
+# - geogeo for å sette lat/long (bruk /kart)
+
+import os
+import sys
+import web
+import time
+import json
+import uuid
+import yaml
+import hashlib
+import logging
+import urllib
+import urllib2
+import gettext
+import datetime
+
+import cStringIO as StringIO
+
+from collections import OrderedDict
+from web import form, template
+from tokyo import cabinet
+
+RESOLVER = "http://data.gbif.no/resolver/"
+
+config = yaml.load(file('config.yaml'))
+
+deepzoom = True
+try:
+    from gi.repository import Vips
+except ImportError:
+    deepzoom = False
+
+db = web.database(dbn='sqlite', db='dugnad.db')
+
+def buildform(key):
+    if not key in config['forms']: raise Exception("Form not found")
+    inputs = []
+    recipe = config['forms'][key]
+    for i in recipe:
+        if not 'name' in i: raise Exception("Needs a name")
+        if not 'type' in i: raise Exception("Needs a type")
+        k = i['name']
+        if i['type'] == "text":
+            inputs.append(form.Textbox(k, description = _(k)))
+        elif i['type'] == "checkbox":
+            inputs.append(form.Checkbox(k, value = "y", description = _(k)))
+        elif i['type'] == 'select':
+            if 'options' in i:
+                options = [''] + i['options']
+                inputs.append(form.Dropdown(k, options, description = _(k)))
+            elif 'source' in i:
+                key = i['source'] + "-" + i['name']
+                source = json.loads(web.ctx.metadata.get(key))
+                options = [''] + [d['label'] for d in sorted(source)]
+                inputs.append(form.Dropdown(k, options, description = _(k)))
+	elif i['type'] == 'textfield':
+	    inputs.append(form.Textarea(k, description = _(k)))
+	elif i['type'] == 'date':
+	    inputs.append(form.Textbox("year", description = _('year')))
+	    inputs.append(form.Textbox("month", description = _('month')))
+	    inputs.append(form.Textbox("day", description = _('day')))
+        else: raise Exception("Unknown type %s" % i['type'])
+    return form.Form(*inputs)
+
+class index:
+    def GET(self):
+        return render.index()
+
+class collection:
+    def GET(self, key):
+        return render.collection(key)
+
+class transcribe:
+    def GET(self):
+        return render.transcribe(key, record, forms, zoom)
+
+class annotate:
+    def GET(self, key):
+        url = "%s%s.json" % (RESOLVER, key)
+        record = json.loads(urllib2.urlopen(url).read())
+        date = record.get('dwc:eventDate')
+
+        zoom = False
+        if record.get("dwc:associatedMedia"):
+            source = urllib.unquote(record.get('dwc:associatedMedia').strip())
+            imagekey = hashlib.sha256(source).hexdigest()
+            if deepzoom and source.find("http://www.unimus.no/") == 0:
+                name = "static/tmp/" + imagekey
+                if not os.path.exists(name + "_files"):
+                    urllib.urlretrieve(source, "%s.jpg" % name)
+                    image = Vips.Image.new_from_file("%s.jpg" % name)
+                    image.dzsave(name, layout="dz")
+                zoom = True
+
+        if date:
+            record['year'], record['month'], record['day'] = date.split("-")
+	forms = OrderedDict((form, buildform(form)) for form in config['annotate']['order'])
+        for _, form in forms.iteritems(): form.validates(record)
+        return render.transcribe(key, record, forms, zoom)
+
+    def POST(self, key):
+        uid = session.get('id')
+        url = "%s%s.json" % (RESOLVER, key)
+        record = json.loads(urllib2.urlopen(url).read())
+        today = str(datetime.date.today())
+        db.insert('annotations',
+            id=str(uuid.uuid4()), key=key, user=uid, date=today,
+            annotation=json.dumps(web.input()),
+            original=json.dumps(record)
+        )
+
+urls = (
+    '/', 'index',
+    '/collection/(.+)', 'collection',
+    '/transcribe/(.+)', 'transcribe',
+    '/(.+)', 'annotate',
+)
+
+languages = ['en_US']
+gettext.install('messages', 'lang', unicode=True)
+for lang in languages:
+    gettext.translation('messages', 'lang', languages = [lang]).install(True)
+
+render = template.render('templates', base='layout', globals= { '_': _ })
+
+app = web.application(urls, locals())
+session = web.session.Session(app, web.session.DiskStore('sessions'))
+
+if __name__ == "__main__":
+    app.run()
+
---
+++ static/dugnad.js
@@ -0,0 +1,168 @@
+var T = {};
+
+T.checkcoordinates = function(e) {
+  var latitude = document.getElementsByName('latitude')[0];
+  var longitude = document.getElementsByName('longitude')[0];
+  latitude.className = ''; longitude.className = '';
+  T.map.removeLayer(T.marker);
+
+  var degexp = /^([\d\-.*,°d\s]+)[\s'm]*([nsew])/i
+  var rawlat = latitude.value.match(degexp);
+  var rawlon = longitude.value.match(degexp);
+  if(rawlat && rawlon) {
+    var lat = degtodec(rawlat[2], rawlat[1]);
+    var lon = degtodec(rawlon[2], rawlon[1]);
+    var acc = precision(rawlat[1], rawlon[1]);
+
+    T.marker.setRadius(acc);
+    T.marker.setLatLng([lat, lon]);
+    T.marker.addTo(T.map);
+    switch(acc) {
+      case 30:
+        T.map.setZoom(13); break;
+      case 1847:
+        T.map.setZoom(11); break;
+      case 110874:
+        T.map.setZoom(8); break;
+    }
+    latitude.className = 'ok'; longitude.className = 'ok';
+    T.map.panTo(T.marker.getLatLng());
+  }
+}
+
+T.toggle = function(id, targetId, on, off) {
+  var toggler = document.getElementById(id);
+  var target = document.getElementById(targetId);
+  if(toggler && target) {
+    toggler.onclick = function() {
+      if(target.style.display == 'block') {
+        target.style.display = 'none';
+        document.cookie = targetId + '=0';
+        toggler.textContent = 'Show ' + targetId + '…';
+        if(off) off();
+      } else {
+        target.style.display = 'block';
+        document.cookie = targetId + '=1';
+        toggler.textContent = 'Hide ' + targetId + '…';
+        if(on) on();
+      }
+    }
+  }
+  if(getcookie(targetId) == '1')
+    toggler.click();
+}
+
+T.help = function(e) {
+  var help = document.getElementById('help');
+  var text = e.target.getAttribute('data-help');
+  if(help && text)
+    help.innerHTML = text;
+}
+
+T.next = function(e) {
+  if(e.keyCode == 13 || e.which == 13) {
+    e.preventDefault();
+    return false;
+  }
+  return true;
+}
+
+T.validate = function(e) {
+  var el = e.target;
+  if(el.name == 'latitude' || el.name == 'longitude') return;
+  el.className = '';
+  if(!el.value || el.value == '')
+    return;
+
+  switch(el.name) {
+    case 'elevation':
+      if(!el.value.match(/\d/))
+        el.className = 'error';
+      break;
+    case 'month':
+      if(el.value.match(/\d/) && (el.value > 12 || el.value < 1))
+        el.className = 'error';
+      break;
+    case 'day':
+      var day = parseInt(el.value);
+      if(!el.value.match(/\d/) || day < 1 || day > 31)
+        el.className = 'error';
+      break;
+  }
+}
+
+function getcookie(name) {
+  var id = name + '=';
+  var cookies = document.cookie.split(';');
+  for(var i=0; i < cookies.length; i++) {
+    var cookie = cookies[i].trim();
+    if(cookie.indexOf(id) == 0)
+      return cookie.substring(id.length, cookie.length);
+  }
+  return '';
+}
+
+function degtodec(nsew, coords) {
+  var parts = coords.split(/[-.*°d\s]/);
+  var degrees = parseInt(parts[0]), minutes = parts[1], seconds = parts[2];
+  if(minutes)
+    degrees = degrees + (parseInt(minutes) / 60);
+  if(seconds)
+    degrees = degrees + (parseInt(seconds) / 3600);
+  if(nsew.match(/[ws]/i))
+    return -degrees;
+  return degrees;
+}
+
+function precision(lat, lon) {
+  var parts = lat.split(/[-.*°d\s]/);
+  var degrees = parseInt(parts[0]), minutes = parts[1], seconds = parts[2];
+  if(seconds) return 30;
+  if(minutes) return 1847;
+  return 110874;
+}
+
+document.addEventListener("DOMContentLoaded", function(e) {
+  window.ActiveXObject = null;
+
+  var url = 'http://otile2.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.jpg';
+  var att = 'Data, imagery and map information provided by MapQuest, <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> and contributors, <a href="http://wiki.openstreetmap.org/wiki/Legal_FAQ#I_would_like_to_use_OpenStreetMap_maps._How_should_I_credit_you">ODbL</a>.';
+  var osm = new L.TileLayer(url, { minZoom: 1, maxZoom: 16, attribution: att });
+
+  T.map = L.map('map').setView([0, 0], 2);
+  T.map.addLayer(osm);
+
+  T.toggle('toggle-help', 'help');
+  T.toggle('toggle-map', 'map', function() {
+    T.map.invalidateSize(false);
+  });
+
+  var form = document.getElementById('transcription');
+  if(form) {
+    for(var i=0; i < form.length; i++) {
+      form.elements[i].addEventListener('keypress', T.next, false);
+      form.elements[i].addEventListener('change', T.validate, false);
+      form.elements[i].addEventListener('focus', T.help, false);
+    }
+    var save = document.getElementsByName('save')[0];
+    save.onclick = function(e) {
+      var nodate = document.getElementsByName('nodate')[0];
+      if(nodate.checked)
+        return true;
+      var year = document.getElementsByName('year')[0];
+      if(year && !year.value || year.value == "")
+        return false;
+    }
+  }
+
+  var latitude = document.getElementsByName('latitude')[0];
+  var longitude = document.getElementsByName('longitude')[0];
+  if(latitude && longitude) {
+    T.marker = L.circle([0, 0], 1000);
+    latitude.onkeyup = T.checkcoordinates;
+    longitude.onkeyup = T.checkcoordinates;
+  }
+  document.getElementsByName("province")[0].focus();
+
+});
+
