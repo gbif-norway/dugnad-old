1639f712cf532aab05911fa2f43f2db67921f2cf
bie <bie@blekksprut.net>
2017-05-18 23:21:23 +0900

    progress bar-test

---
+++ static/js/info.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function(e) {
+  var subs = document.getElementsByClassName('subproject-thumbnail');
+  for(var i = 0; i < subs.length; i++) {
+    var progress = subs[i].getAttribute('data-progress');
+    var gradient = "linear-gradient(to bottom, white " + progress + "%, rgba(0, 150, 0, 0.7))";
+    subs[i].style.backgroundImage = gradient;
+  }
+});
