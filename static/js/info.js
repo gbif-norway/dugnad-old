57c8924def6e233b2409d2e0854b8b7c672f9255
bie <bie@blekksprut.net>
2017-05-18 23:25:04 +0900

    snu progress

--- static/js/info.js
+++ static/js/info.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(e) {
   var subs = document.getElementsByClassName('subproject-thumbnail');
   for(var i = 0; i < subs.length; i++) {
-    var progress = subs[i].getAttribute('data-progress');
+    var progress = 100 - subs[i].getAttribute('data-progress');
     var gradient = "linear-gradient(to bottom, white " + progress + "%, rgba(0, 150, 0, 0.7))";
     subs[i].style.backgroundImage = gradient;
   }
